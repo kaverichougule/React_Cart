1b78b2e68c2189bdf2bad9c8d8b258819d2f6588
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-12-26 03:04:46 +0530

    Initial Commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,68 @@
 import "./Body.css";
 import data from "./data.json";
 import SingleItem from "./SingleItem";
+import { useState } from "react"; 
 export default function Body() {
+  const [totalPrice, setTotalPrice] = useState(2199.96);
+  const [cartItems, setCartItems] = useState(data);
+  const [isCartEmpty, setIsCartEmpty] = useState(cartItems.length === 0);
+
+  function handleUpdateTotal(title, newCount) {
+    // Update the count for the specific item
+    const updatedCartItems = cartItems.map((item) =>
+      item.title === title ? { ...item, count: newCount } : item
+    );
+
+    // Recalculate the total price
+    const newTotalPrice = updatedCartItems.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+
+    // Update both the cart items and total price
+    setCartItems(updatedCartItems);
+    setTotalPrice(newTotalPrice);
+  }
+
+  function handleRemoveItem(title, count) {
+    // Remove the item from the cartItems state based on the title and count
+    const updatedCartItems = cartItems.filter(
+      (item) => !(item.title === title && item.count === count)
+    );
+    setCartItems(updatedCartItems);
+
+    // Recalculate the total price
+    const newTotalPrice = updatedCartItems.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+    setTotalPrice(newTotalPrice);
+
+    // Check if the cart is empty
+    setIsCartEmpty(updatedCartItems.length === 0);
+  }
+
+  function clearCart() {
+    // Clear the cart and update total price
+    setCartItems([]);
+    setTotalPrice(0);
+    setIsCartEmpty(true);
+  }
   return (
     <div className="mainContainer">
       <div className="container">
         <div className="items">
-          {data.map((items)=>{
-            return <SingleItem title={items.title} img={items.img} price={items.price} />
+          {cartItems.map((items) => {
+            return (
+              <SingleItem
+                title={items.title}
+                img={items.img}
+                price={items.price}
+                onRemove={handleRemoveItem}
+                onUpdateTotal={handleUpdateTotal}
+                count={items.count}
+              />
+            );
           })}
         </div>
       </div>
@@ -26,7 +81,13 @@ export default function Body() {
         <div className="totalSum">
           <div className="total">
             <p className="totalHeading">Total</p>
-            <p>$11,111</p>
+            <p>
+              $
+              {("" + totalPrice).substring(
+                0,
+                ("" + totalPrice).indexOf(".") + 3
+              )}
+            </p>
           </div>
         </div>
       </div>
--- src/Components/SingleItem.js
+++ src/Components/SingleItem.js
@@ -1,7 +1,43 @@
 import "./Body.css";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 export default function SingleItem(props){
-    let [Increasecount,setInc]=useState(1);
+    let [count, setCount] = useState(props.count);
+
+  useEffect(() => {
+    // Update the count when the props.count changes
+    setCount(props.count);
+  }, [props.count]);
+
+  function increaseCount() {
+    setCount(count + 1);
+    props.onUpdateTotal(props.title, count + 1);
+  }
+
+  function decreaseCount() {
+    if (count > 0) {
+        const newCount = count - 1;
+  
+        // If the count reaches 0, remove the item immediately
+        if (newCount === 0) {
+          props.onRemove(props.title, 1);
+        } else {
+          setCount(newCount);
+        }
+      }
+  }
+
+  function removeItem() {
+    // Call the onRemove callback with the item's title and count
+    props.onRemove(props.title, count);
+  }
+
+  // Add a function to update the total price in the parent component
+  function updateTotalPrice(newCount) {
+    // Calculate the new total price based on the updated count
+    const newTotalPrice = newCount * props.price;
+    // Call a callback function to update the total price in the parent component
+    props.onUpdateTotal(newTotalPrice);
+  }
     return(
         <div className="singleItem">
             <div className="singleItemInfo">
@@ -12,20 +48,26 @@ export default function SingleItem(props){
               />
               <div className="productName">
                 <p>{props.title}</p>
-                <p>{props.price}</p>
+                <p>${props.price}</p>
               </div>
             </div>
 
             <div className="quantity">
-              <div className="trash">
+              <div className="trash" onClick={removeItem}>
                 <i class="fa-solid fa-trash"></i>
               </div>
               <div className="Subquantity">
-                <div className="IncreaseBtn">
+                <div className="IncreaseBtn" onClick={()=>{
+                    increaseCount()
+                    
+                }}>
                     <i class="fa-solid fa-chevron-up"></i>
                 </div>
-                <p>1</p>
-                <div className="DecreaseBtn">
+                <p >{count}</p>
+                <div className="DecreaseBtn" onClick={()=>{
+                    decreaseCount(); 
+                    
+                }}>
                     <i class="fa-solid fa-chevron-down"></i>
                 </div>
               </div>
